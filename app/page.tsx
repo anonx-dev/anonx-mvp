878913b838e9494cb88e26e82ce4fbbb4020e776
rishu <rock.jade09@gmail.com>
2025-10-16 16:03:46 +0530

    add netlify

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const page = () => {
-  return <div> OM Ganeshay Namah🕉️</div>;
+  return (
+    <div className="text-4xl font-bold h-screen flex flex-col gap-4 items-center justify-center">
+      <h1>ANONX</h1>
+      <h2>COMING SOON!</h2>
+    </div>
+  );
 };
 
 export default page;
